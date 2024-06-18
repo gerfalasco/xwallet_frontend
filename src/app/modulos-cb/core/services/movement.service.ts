2a0661f17e4d4da4bd606cdd9ee2c8745a8b2b85
nguzmanphysis <nguzman@physis.com.ar>
2024-06-18 03:15:40 -0300

    (modulo-cb-movement) - Se crea el servicio de inversion.

--- src/app/modulos-cb/core/services/movement.service.ts
+++ src/app/modulos-cb/core/services/movement.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { SettingsService } from 'src/app/services/settings.service';
 import { IIntercambio } from '../interfaces/intercambio.interface';
 import { IAccount } from '../interfaces/account.interface';
+import { IInversion } from '../interfaces/inversion.interface';
 
 @Injectable()
 export class MovementService {
@@ -12,7 +13,7 @@ export class MovementService {
     private readonly settingsService: SettingsService,
   ) { }
 
-  setMovement({ intercambio }: { intercambio: IIntercambio}) {
+  exchange({ intercambio }: { intercambio: IIntercambio}) {
     if (intercambio) {
       const url = `${this.settingsService.api}api/v1/operations/exchanges`;
 
@@ -22,6 +23,29 @@ export class MovementService {
         "originCustomerId": intercambio.originCustomerId,
         "transactionType": intercambio.transactionType,
         "amount": intercambio.amount
+      };
+
+      return this.httpClient.post<IAccount>(url, body);
+    }
+
+    else {
+      const errorResponse = {
+        titulo: "Error Movimiento",
+        message: "el movimiento debe tener un valor."
+      };
+      throw new Error(JSON.stringify(errorResponse));
+    }
+  }
+
+  investment({ inversion }: { inversion: IInversion}) {
+    if (inversion) {
+      const url = `${this.settingsService.api}api/v1/operations/investments`;
+
+      const body = {
+        "accountId": inversion.accountId,
+        "customerId": inversion.customerId,
+        "investmentDays": inversion.investmentDays,
+        "investmentAmount": inversion.investmentAmount
       };
 
       return this.httpClient.post<IAccount>(url, body);
