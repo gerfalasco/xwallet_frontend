223b7fbe1f24340cdffbf80bb7b812208089e1e4
nguzmanphysis <nguzman@physis.com.ar>
2024-06-24 02:05:33 -0300

    (modulo-core-movement) - Se agrega transferencia y transacción.

--- src/app/modulos-cb/core/services/movement.service.ts
+++ src/app/modulos-cb/core/services/movement.service.ts
@@ -4,10 +4,11 @@ import { SettingsService } from 'src/app/services/settings.service';
 import { IIntercambio } from '../interfaces/intercambio.interface';
 import { IAccount } from '../interfaces/account.interface';
 import { IInversion } from '../interfaces/inversion.interface';
+import { ITransaccion } from '../interfaces/transaccion.interface';
+import { ITransferencia } from '../interfaces/transferencia.interface';
 
 @Injectable()
 export class MovementService {
-
   constructor(
     private readonly httpClient: HttpClient,
     private readonly settingsService: SettingsService,
@@ -46,6 +47,53 @@ export class MovementService {
         "customerId": inversion.customerId,
         "investmentDays": inversion.investmentDays,
         "investmentAmount": inversion.investmentAmount
+      };
+
+      return this.httpClient.post<IAccount>(url, body);
+    }
+
+    else {
+      const errorResponse = {
+        titulo: "Error Movimiento",
+        message: "el movimiento debe tener un valor."
+      };
+      throw new Error(JSON.stringify(errorResponse));
+    }
+  }
+  
+  transaction({transaccion}: { transaccion: ITransaccion; }) {
+    if (transaccion) {
+      const url = `${this.settingsService.api}api/v1/operations/transactions`;
+
+      const body = {
+        "accountId": transaccion.accountId,
+        "customerId": transaccion.customerId,
+        "transactionType": transaccion.transactionType,
+        "amount": transaccion.amount
+      };
+
+      return this.httpClient.post<IAccount>(url, body);
+    }
+
+    else {
+      const errorResponse = {
+        titulo: "Error Movimiento",
+        message: "el movimiento debe tener un valor."
+      };
+      throw new Error(JSON.stringify(errorResponse));
+    }
+  }
+
+  transfer({transferencia}: { transferencia: ITransferencia; }) {
+    if (transferencia) {
+      const url = `${this.settingsService.api}api/v1/operations/transfers`;
+
+      const body = {
+        "originAccountId": transferencia.originAccountId,
+        "destinationAccountId": transferencia.destinationAccountId,
+        "originCustomerId": transferencia.originCustomerId,
+        "transactionType": transferencia.transactionType,
+        "amount": transferencia.amount
       };
 
       return this.httpClient.post<IAccount>(url, body);
