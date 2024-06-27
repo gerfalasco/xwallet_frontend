b2f460b506e62606f5b0fccded43aa0b164f291c
gerfalasco <gerfalasco@gmail.com>
2024-06-27 18:55:29 -0300

    (modulos-cb-transaccion) - Se modifica el monto. - Se modificac transaccion por tipo.

--- src/app/modulos-cb/transaccion/transaccion.module.ts
+++ src/app/modulos-cb/transaccion/transaccion.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IngresoComponent } from './views/ingreso/ingreso.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DecimalesPipe } from '@cb/core/pipes/decimales.pipe';
+import { PipeModule } from '@cb/core/pipes';
 
 
 
@@ -13,6 +15,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-  ]
+    PipeModule,
+  ],
+  providers: [DecimalesPipe]
 })
 export class TransaccionModule { }
